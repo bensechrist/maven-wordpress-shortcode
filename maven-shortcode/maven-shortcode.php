9a4bc6fcd00249c83ebc5f2087c41feade490ead
Ben <bensechrist@gmail.com>
2014-10-10 10:11:48 -0400

    Initial Commit

---
+++ maven-shortcode/maven-shortcode.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Plugin Name: Maven Shortcode
+ * Plugin URI: http://example.com
+ * Description: A shortcode plugin that injects maven dependencies
+ * Version: 1.0
+ * Author: Ben Sechrist
+ * Author URI: http://boffin.sechristfamily.com
+ * License: GPL2
+ */
+
+defined('ABSPATH') or die("No script kiddies please!");
+
+function inject_maven_dependency($atts) {
+	if (empty($atts['groupid']))
+		return "Group ID needed<br />";
+	if (empty($atts['artifactid']))
+		return "Artifact ID needed<br />";
+	if (empty($atts['version']))
+		$atts['version'] = "";
+
+	$json_string = file_get_contents("http://search.maven.org/solrsearch/select?q=g:%22" 
+		. $atts['groupid'] . "%22%20AND%20a%3A%22" . $atts['artifactid'] 
+		. "%22%20AND%20v%3A%22" . $atts['version'] . "%22&rows=1&wt=json");
+	$json = json_decode($json_string);
+	$first_result = $json->response->docs[0];
+
+	$output = "&lt;dependency&gt;<br />";
+	$output .= "&nbsp;&nbsp;&lt;groupId&gt;" . $first_result->g 
+		. "&lt;/groupId&gt;<br />";
+	$output .= "&nbsp;&nbsp;&lt;artifactId&gt;" . $first_result->a 
+		. "&lt;/artifactId&gt;<br />";
+	if (!empty($first_result->latestVersion))
+		$output .= "&nbsp;&nbsp;&lt;version&gt;" . $first_result->latestVersion 
+			. "&lt;/version&gt;<br />";
+	else
+		$output .= "&nbsp;&nbsp;&lt;version&gt;" . $first_result->v 
+			. "&lt;/version&gt;<br />";
+	$output .= "&lt;/dependency&gt;<br />";
+	$output .= "<br />";
+
+	return $output;
+}
+
+add_shortcode('mvn-dependency', 'inject_maven_dependency');
+
+function inject_maven_version($atts) {
+	$json_string = file_get_contents("http://search.maven.org/solrsearch/select?q=g:%22" 
+		. $atts['groupid'] . "%22%20AND%20a%3A%22" . $atts['artifactid'] . "%22&rows=1&wt=json");
+	$json = json_decode($json_string);
+	$first_result = $json->response->docs[0];
+
+	return $first_result->latestVersion;
+}
+
+add_shortcode('mvn-version', 'inject_maven_version');
